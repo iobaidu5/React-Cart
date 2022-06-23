6e5b48033c4362ce5d0fd3a11b6588bb9dc3bb2a
iobaidu5 <i.obaidu5@gmail.com>
2022-06-23 16:47:16 +0500

    cart state added

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -2,15 +2,33 @@ import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
 const Products = (props) => {
+  const dummyData = [
+    {
+      id: 1,
+      title: "First Book",
+      price: 20,
+      description: "My First Book"
+    },
+    {
+      id: 2,
+      title: "Second Book",
+      price: 40,
+      description: "My Second Book"
+    }
+  ];
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
+      {dummyData.map((item) => (
         <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        key={item.id}
+        id={item.id}
+        title={item.title}
+        price={item.price}
+        description={item.description}
+      />
+      ))};
       </ul>
     </section>
   );
